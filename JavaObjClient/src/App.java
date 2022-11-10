84f0c823fc6261ac9317b046ab5af45b0c15b8ba
ss35789 <ss35789@naver.com>
2022-11-10 14:14:57 +0900

    .

---
+++ JavaObjClient/src/App.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class App extends JFrame{
+    private JPanel AppSidebar;
+    private JPanel panel1;
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
