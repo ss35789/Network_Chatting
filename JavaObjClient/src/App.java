bb62da1146a9bf9d4b2d583fa36b407a55ad2054
ss35789 <ss35789@naver.com>
2022-11-10 17:25:57 +0900

    chatting 리스트 페이지 생성
    메인 페이지에서 chatting 버튼 누르면 페이지 이동

--- JavaObjClient/src/App.java
+++ JavaObjClient/src/App.java
@@ -8,7 +8,12 @@ public class App extends JFrame{
     private JButton chattingRoomButton;
     private JLabel myName;
 
+    private void ChattingButton_Click(JFrame App){
+       //chattingPanel·Î ÀÌµ¿
 
+        JFrame cha = new Chatting();
+        App.setVisible(false);
+    }
     private void createUIComponents() {
         // TODO: place custom component creation code here
         mainPanel = new JPanel();
@@ -16,14 +21,18 @@ public class App extends JFrame{
     }
     public App(String title){
         super(title);
+        chattingRoomButton.addActionListener(event -> ChattingButton_Click(this));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+        this.setVisible(true);
 
     }
 
+
+
     public static void main(String[] args) {
         JFrame frame = new App("NetWork Chatting");
-        frame.setVisible(true);
+
     }
 }
--- JavaObjClient/src/Chatting.java
+++ JavaObjClient/src/Chatting.java
@@ -1,2 +1,29 @@
-public class Chatting {
+import javax.swing.*;
+
+public class Chatting extends JFrame{
+    private JPanel chattingPanel;
+    private JPanel mainPanel;
+    private JButton userButton;
+    private JPanel sideBarPanel;
+    private JButton chattingRoomButton;
+    private JLabel myName;
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        mainPanel = new JPanel();
+
+    }
+    public Chatting(){
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new App("NetWork Chatting");
+
+    }
 }
