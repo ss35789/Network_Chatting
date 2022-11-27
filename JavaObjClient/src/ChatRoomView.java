097ac0ceb4e6e68982cad38db60d7c5ef6b1c3ba
ss35789 <ss35789@naver.com>
2022-11-27 11:52:11 +0900

    ChatRoom +, send 버튼 이벤트 추가

--- JavaObjClient/src/ChatRoomView.java
+++ JavaObjClient/src/ChatRoomView.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ChatRoomView extends JFrame{
     private JPanel mainPanel;
@@ -10,13 +13,69 @@ public class ChatRoomView extends JFrame{
     private JLabel lblRoomUserNum;
     private JLabel btnUserList;
     private JLabel btnSubmit;
-    private JTextArea textArea1;
+    private JLabel btnSendImg;
 
     public ChatRoomView(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setContentPane(mainPanel);
 
+        btnSendImg.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                //마우스 이벤트
+                setContentPane(mainPanel);
+                revalidate();
+                repaint();
+
+
+                setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+
+                setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+        });
+
+        btnSubmit.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                //마우스 이벤트
+                setContentPane(mainPanel);
+                revalidate();
+                repaint();
+
+
+                setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+
+                setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+        });
     }
+
+
+
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
